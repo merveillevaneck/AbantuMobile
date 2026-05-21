f6a82dde9fc306bbffac32e63d4046e079378cf7
merveillevaneck <merveillevaneck@gmail.com>
2026-05-21 20:43:47 +0200

    fixed api origin again

--- src/server/api/client.ts
+++ src/server/api/client.ts
@@ -4,7 +4,7 @@ import { getToken } from '@/store/token';
 //const API = "http://localhost:3001";
 //const API = "http://192.168.0.104:3000";
 // const API = "https://mervstation.tail4f070.ts.net";
-const API = "https://merveilles-macbook-air.tail4f070.ts.net/abantube";
+const API = "https://mervstation.tail4f070.ts.net/abantube";
 
 export const apiClient = createApiClient(API)
 
